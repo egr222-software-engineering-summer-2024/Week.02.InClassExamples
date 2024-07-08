948c8598cd172293761e9abc41cd3be061c076e5
Larry Clement <lclement@calbaptist.edu>
2024-07-08 11:42:39 -0700

    After completing demo

--- A/Mirror/src/Mirror.java
+++ A/Mirror/src/Mirror.java
@@ -9,14 +9,42 @@ public class Mirror {
     }
 
     public static void border() {
-        System.out.println("border");
+        System.out.print("#");
+        for (int equal=1; equal <= (4 * SIZE); equal++) {
+            System.out.print("=");
+        }
+        System.out.println("#");
     }
 
     public static void topHalf() {
-        System.out.println("topHalf");
+        for (int line=1; line<=SIZE; line++) {
+            // contents of each line
+            System.out.print("|");
+            printString(" ", (-2 * line + (2 * SIZE)));
+            System.out.print("<>");
+            printString(".", (4 * line - 4));
+            System.out.print("<>");
+            printString(" ", (-2 * line + (2 * SIZE)));
+            System.out.println("|");
+        }
     }
 
     public static void bottomHalf() {
-        System.out.println("bottomHalf");
+        for (int line=SIZE; line>=1; line--) {
+            // contents of each line
+            System.out.print("|");
+            printString(" ", (-2 * line + (2 * SIZE)));
+            System.out.print("<>");
+            printString(".", (4 * line - 4));
+            System.out.print("<>");
+            printString(" ", (-2 * line + (2 * SIZE)));
+            System.out.println("|");
+        }
+    }
+
+    public static void printString(String str, int count) {
+        for (int i=1; i<=count; i++) {
+            System.out.print(str);
+        }
     }
 }
